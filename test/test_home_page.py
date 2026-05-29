ac1d3c2f4c355929a5e83cf31ac776a628bb0979
yoolalw <larakemilyprofissional@gmail.com>
2026-05-29 11:32:36 -0300

    aa

--- test/test_home_page.py
+++ test/test_home_page.py
@@ -11,7 +11,8 @@ import time
 def chrome():
     service = webdriver.ChromeService()
     chrome = webdriver.Chrome(service=service)
-    
+    chrome.implicitly_wait(3)
+
     yield chrome
     chrome.quit()
 
@@ -20,6 +21,7 @@ def test_itens_displayed(chrome):
 
     chrome.get('http://127.0.0.1:5500/home.html')
 
+    time.sleep(3)
     chrome.find_element(By.ID, "buttonCa").is_displayed()
     chrome.find_element(By.ID, "buttonKb").is_displayed()
     chrome.find_element(By.ID, "rel").is_displayed()
@@ -42,10 +44,20 @@ def test_verify_msg(chrome):
     chrome.get('http://127.0.0.1:5500/ca.html')
 
     input = chrome.find_element(By.ID, "input")
+    time.sleep(2)
     btn = chrome.find_element(By.ID, "button").click()
 
     time.sleep(1)
     msg = chrome.find_element(By.ID, "msg")
     assert msg.text == "O campo precisa estar preenchido."
 
+
+@pytest.mark.usefixtures("chrome")
+def test_inserting(chrome):
+
+    chrome.get('http://127.0.0.1:5500/ca.html')
+
+    input = chrome.find_element(By.ID, "input").send_keys("Atividade 1")
+    time.sleep(2)
+    btn = chrome.find_element(By.ID, "button").click()
 
