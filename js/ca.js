72aa041c14cd4886b5dc7fa7d4fda39a5c0448d6
yoolalw <larakemilyprofissional@gmail.com>
2026-05-29 09:56:59 -0300

    salvando item unico no local storage

--- js/ca.js
+++ js/ca.js
@@ -1,1 +1,29 @@
 console.log("a")
+
+const msg = document.getElementById("msg")
+const button = document.getElementById("button")
+
+button.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const input = document.getElementById("input").value
+
+    if (input.value == "") {
+        console.log("b")
+        msg.textContent = "O campo precisa estar preenchido."
+        
+    } else {
+        console.log("c")
+        msg.textContent = ""
+
+        const item = [{ 
+            atv: JSON.stringify(input),
+            status: "todo"
+        }]
+        const ativ = JSON.parse(localStorage.getItem('ativ')) || []
+        ativ.push(item.atv, item.status)
+        localStorage.setItem('ativ', JSON.stringify(item))
+    }
+})
+
+console.log(localStorage.getItem('ativ'))
